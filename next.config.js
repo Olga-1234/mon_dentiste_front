ff3ba827db439048d775b80a29fc11d19739de7c
Olga Mujinga <monaolgita@gmail.com>
2023-04-17 17:24:21 +0100

    refactor next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-  env: {},
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-  
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true
+    },
+    reactStrictMode: true,
+    env: {},
+    images: {
+        domains: ['res.cloudinary.com']
+    },
+    staticPageGenerationTimeout: 1000
 };
-
-
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
